b620e389ed984b091df40d248b1c7a658437cea8
Brian_P <32593931+spdfnet@users.noreply.github.com>
2019-03-14 14:32:28 +0000

    RBD client parameters, removed wrong values from upstream

--- hooks/nova_compute_context.py
+++ hooks/nova_compute_context.py
@@ -325,8 +325,6 @@ class NovaComputeCephContext(context.CephContext):
             # We use write-though only to be safe for migration
             ctxt['rbd_client_cache_settings'] = \
                 {'rbd cache': 'true',
-                 'rbd cache size': '64 MiB',
-                 'rbd cache max dirty': '0 MiB',
                  'rbd cache writethrough until flush': 'true',
                  'admin socket': '/var/run/ceph/rbd-client-$pid.asok'}
 
